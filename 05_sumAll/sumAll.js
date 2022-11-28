1a11f0c5e7200ac5a69c47fccde0ec8994e271ae
Juan <116412382+jsf97@users.noreply.github.com>
2022-11-28 18:55:53 -0300

    Hecho el ejercicio 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  let minNum = Math.min(x, y)
+  let maxNum = Math.max(x, y)
+  let sumaTotal = 0;
+  
+  if (x < 0 || y < 0 || typeof x  != "number" || typeof y  != "number") {
+    sumaTotal = "ERROR"
+  }
 
-};
-
+  else for (i = minNum; i <= maxNum; i++) {
+    sumaTotal += i;
+  }
+  return sumaTotal 
+  }
 // Do not edit below this line
 module.exports = sumAll;

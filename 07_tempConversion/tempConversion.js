371a87ac7333ad0040255c8a7d70cc647c56e665
Juan <116412382+jsf97@users.noreply.github.com>
2022-11-29 17:31:33 -0300

    Resuelto.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let nmb = (number - 32) * .5556
+  return (Math.round(nmb * 10) / 10)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let nmb = number * 1.8 + 32
+  return (Math.round(nmb * 10) / 10)
 };
 
 // Do not edit below this line
